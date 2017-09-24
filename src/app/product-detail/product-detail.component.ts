6d4bcd544ea245f9ec183bdd58ec33de2a9e52dc
feixia5320 <1044489176@qq.com>
2017-09-24 22:36:22 +0800

    添加评论功能，使用双向绑定

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -11,6 +11,10 @@ export class ProductDetailComponent implements OnInit {
 
   product: Product;
   comments: Comment[];
+  newRating = 5;
+  newComment = "";
+  isCommentHidden = true;
+
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService
               ) { }
@@ -20,5 +24,15 @@ export class ProductDetailComponent implements OnInit {
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
   }
+  addComment(){
+    let comment = new Comment(0,this.product.id, new Date().toISOString(),"niuboabao",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
 
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+
+  }
 }
