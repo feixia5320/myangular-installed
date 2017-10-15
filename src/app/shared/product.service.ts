1072f91b180946569fae06d47dc9eed9b9622c41
feixia5320 <1044489176@qq.com>
2017-10-15 22:47:53 +0800

    响应式表单验证

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -19,6 +19,10 @@ export class ProductService {
   ];
   constructor() { }
 
+  getAllCategories(): string[] {
+    return ['电子产品', '通讯设备', '图书'];
+  };
+
   getProducts(): Product[] {
     return this.products;
   }
